15971e83e2a21678798b0b40168887f1e507627d
Benjamin Allen <110572177+benwillallen@users.noreply.github.com>
2025-02-02 07:47:58 -0600

    Finalized Commit and Push
    Signed-off-by: Benjamin Allen <110572177+benwillallen@users.noreply.github.com>

--- Video/Video_Animations.py
+++ Video/Video_Animations.py
@@ -232,13 +232,13 @@ class Animation5DataCleaningFeatureEngineering(Scene):
         dec_graph3 = dec_graph.copy().next_to(dec_graph, RIGHT, buff=-0.15)
 
         # Dropped rows with missing data, normalization, etc.
-        dc_title = Text("Data Cleaning", font_size=50).shift(2.5 * UP)
+        """dc_title = Text("Data Cleaning", font_size=50).shift(2.5 * UP)
         dc_title_border = SurroundingRectangle(dc_title, buff=0.25, stroke_width=10, fill_opacity=0.5)
         dc_text = Paragraph("1. Dropped 24 Rows Containing NA Values",
                             "2. Dropped Non EEG Related Columns",  # Difficult to work with
                             "3. Standardized EEG Data",
                             t2c={"24": "#D62828", "NA": "#D62828", "Non": "#D62828", "Standardized": "#007991"},
-                            font_size=40).shift(DOWN)
+                            font_size=40).shift(DOWN)"""
 
         # Feature Engineering
         # SMOTE, PCA, etc. (can include mathematical/visual descriptions of these methods)
@@ -250,24 +250,18 @@ class Animation5DataCleaningFeatureEngineering(Scene):
                             t2c={"Adjacency Matrix": "#D62828", "PCA": "#1B264F", "UMAP": "#1B264F", "SMOTE": "#1B264F"},
                             font_size=37).shift(DOWN)
 
-        """self.play(Write(VGroup(dec_graph2, dec_graph, dec_graph3)), Create(dc_title_border), Write(dc_title))
-        self.wait(1)
-        self.play(Write(dc_text))
-        self.wait(1)
-        self.play(FadeOut(dc_text, shift=DOWN))
-        self.wait(1)
-        self.play(Transform(dc_title_border, fe_title_border), Transform(dc_title, fe_title))
+        self.play(Write(VGroup(dec_graph2, dec_graph, dec_graph3)), Create(fe_title_border), Write(fe_title))
         self.wait(1)
         self.play(Write(fe_text))
-        self.wait(1)"""
+        self.wait(1)
 
         # Can add more here explaining the adjacency matrix stuff
         adj_title = Text("Constructing Graph", font_size=50).shift(3 * UP)
         adj_title_border = SurroundingRectangle(adj_title, buff=0.25, stroke_width=10, fill_opacity=0.5)
-        adj_step1_text = Text("1. Find maximum coherence across bands", font_size=40).shift(3*DOWN)
-        adj_step2_text = Text("2. Set max coherence as weight and keep top 10", font_size=40).shift(3*DOWN)
-        adj_step3_text = Text("3. Construct graph using pytorch-geometric", font_size=40).shift(3*DOWN)
-        adj_explain_text = Text("Coherence is used to understand structure of brain", font_size=40).shift(3*DOWN)
+        adj_step1_text = Text("1. Find maximum coherence across bands", font_size=40).shift(3.25*DOWN)
+        adj_step2_text = Text("2. Set max coherence as weight and keep top 10", font_size=40).shift(3.25*DOWN)
+        adj_step3_text = Text("3. Construct graph using pytorch-geometric", font_size=40).shift(3.25*DOWN)
+        adj_explain_text = Text("Coherence is used to understand structure of brain", font_size=40).shift(3.25*DOWN)
         brain = Ellipse(width=3, height=4, stroke_width=10, stroke_color="#D62828", fill_color="#DF5353",
                         fill_opacity=0.5).scale(1.15)
         ear_left = Circle(radius=0.5, stroke_width=10, stroke_color="#D62828", fill_color="#DF5353", fill_opacity=0.5) \
@@ -312,12 +306,13 @@ class Animation5DataCleaningFeatureEngineering(Scene):
         bands_table_border = SurroundingRectangle(bands_table, buff=0, fill_opacity=0.15, stroke_color="#21201E")
         main_table_border = SurroundingRectangle(bands_table_border, stroke_width=10, fill_opacity=0.75)
         bands_table_group = VGroup(main_table_border, bands_table, bands_table_border)\
-            .next_to(ear_right, RIGHT, buff=0.5)
+            .next_to(ear_right, RIGHT, buff=0.75)
         pytorch_text = Paragraph("Pytorch-Geometric",
-                                 "Graph", fontsize=50).move_to(bands_table_group)
+                                 "Graph", font_size=40).move_to(bands_table_group).scale(0.75)
         pytorch_border = SurroundingRectangle(pytorch_text, stroke_width=10, fill_opacity=0.75)
         pytorch_arrow = Arrow(start=ear_right.get_right(), end=pytorch_border.get_left())
-        self.play(Transform(dc_title_border, adj_title_border), Transform(dc_title, adj_title))
+        self.play(Transform(fe_title_border, adj_title_border), Transform(fe_title, adj_title),
+                  Unwrite(fe_text))
         self.wait(1)
         self.play(AnimationGroup(DrawBorderThenFill(brain), DrawBorderThenFill(ear_left),
                                  DrawBorderThenFill(ear_right), Write(mix_graph), lag_ratio=0.2))
@@ -325,7 +320,7 @@ class Animation5DataCleaningFeatureEngineering(Scene):
         self.play(ReplacementTransform(mix_graph, eeg_graph, run_time=0.75), Write(bands_table_group))
         self.wait(1)
         self.play(Write(adj_step1_text))
-        weight_label = MathTex("100 COH", font_size=30, color="#0E9594")\
+        weight_label = MathTex("100", font_size=30, color="#21201E")\
             .next_to(eeg_graph.edges[("Fp1", "Fp2")], UP, buff=0.2)
         self.play(Write(weight_label), eeg_graph.edges[("Fp1", "Fp2")].animate.set_opacity(1))
         self.wait(1)
@@ -348,111 +343,26 @@ class Animation5DataCleaningFeatureEngineering(Scene):
         self.wait(1)
         self.play(ReplacementTransform(adj_step3_text, adj_explain_text))
         self.wait(1)
-
-
-class SimpleGraph(Scene):
-    def construct(self):
-        # Define the graph with nodes and edges
-        vertices = ["Fp1", "Fp2"]  # Nodes
-        edges = [("Fp1", "Fp2")]   # Edge connecting Fp1 and Fp2
-
-        # Create the graph
-        graph = Graph(vertices, edges, layout="circular")
-
-        # Customize the appearance of the graph
-        graph["Fp1"].set_color(BLUE)  # Set color of Fp1 node
-        graph["Fp2"].set_color(RED)   # Set color of Fp2 node
-        graph.edges[("Fp1", "Fp2")].set_color(GREEN)  # Set color of the edge
-
-        # Add the graph to the scene
-        self.play(Create(graph))
-        self.wait(2)  # Wait for 2 seconds to display the graph
+        self.play(Unwrite(eeg_graph), Unwrite(adj_explain_text), Unwrite(pytorch_text), Uncreate(pytorch_border),
+                  Uncreate(pytorch_arrow), FadeOut(weight_label), FadeOut(fe_title, fe_title_border, shift=UP),
+                  Unwrite(brain), Unwrite(ear_right), Unwrite(ear_left))
+        self.wait(1)
 
 
 class Animation7Modeling(Scene):
     def construct(self):
         b_plane = set_defaults()
         self.add(b_plane)
-        model_text = Text("Full Model Architecture", font_size=50, color="#FEE20B") \
-            .shift(3.5 * UP)
-        df1_text = Paragraph("Preprocessed", "Dataframe", font_size=35)
-        df2_text = Paragraph("Standardized", "Dataframe", font_size=35)
-        dfs_group = VGroup(df2_text, df1_text).arrange(buff=2).shift(2 * UP)
-        df1_box = SurroundingRectangle(df1_text, stroke_color="#C3423F", fill_color="#C3423F")
-        df2_box = SurroundingRectangle(df2_text, stroke_color="#C3423F", fill_color="#C3423F")
-        xg1_text = Paragraph("General", "XGBoost", font_size=35).scale(0.75)
-        xg2_text = Paragraph("Small-Value", "XGBoost", font_size=35).scale(0.75)
-        xg3_text = Paragraph("Large-Value", "XGBoost", font_size=35).scale(0.75)
-        xg4_text = Paragraph("Extreme-Value", "XGBoost", font_size=35).scale(0.75)
-        xg_group = VGroup(xg4_text, xg3_text, xg2_text, xg1_text).arrange(buff=0.65, direction=LEFT)
-        nn_text = Paragraph("Neural", "Network", font_size=35).scale(0.75)
-        model_group = VGroup(xg_group, nn_text).arrange(buff=0.85, direction=LEFT).next_to(dfs_group, DOWN) \
-            .shift(1.25 * DOWN)
-        xg1_box = SurroundingRectangle(xg1_text, stroke_color="#5BC0EB", fill_color="#5BC0EB")
-        xg2_box = SurroundingRectangle(xg2_text, stroke_color="#5BC0EB", fill_color="#5BC0EB")
-        xg3_box = SurroundingRectangle(xg3_text, stroke_color="#5BC0EB", fill_color="#5BC0EB")
-        xg4_box = SurroundingRectangle(xg4_text, stroke_color="#5BC0EB", fill_color="#5BC0EB")
-        nn_box = SurroundingRectangle(nn_text, stroke_color="#F17105", fill_color="#F17105")
-        blend_text = Paragraph("XGBoost", "Blender", font_size=35).next_to(model_group, DOWN).shift(DOWN)
-        blend_box = SurroundingRectangle(blend_text, stroke_color="#FEE20B", fill_color="#FEE20B")
-        arrow1 = CurvedArrow(start_point=df1_box.get_bottom(), end_point=xg1_box.get_top(), angle=PI / 8,
-                             color="#5BC0EB")
-        arrow2 = CurvedArrow(start_point=df1_box.get_bottom(), end_point=xg2_box.get_top(), angle=PI / 8,
-                             color="#5BC0EB")
-        arrow3 = CurvedArrow(start_point=df1_box.get_bottom(), end_point=xg3_box.get_top(), angle=PI / 8,
-                             color="#5BC0EB")
-        arrow4 = CurvedArrow(start_point=df1_box.get_bottom(), end_point=xg4_box.get_top(), angle=PI / 8,
-                             color="#5BC0EB")
-        arrow5 = CurvedArrow(start_point=df2_box.get_bottom(), end_point=nn_box.get_top(), angle=PI / 8)
-        arrow7 = CurvedArrow(start_point=xg1_box.get_bottom(), end_point=blend_box.get_top(), angle=PI / 8,
-                             color="#FEE20B")
-        arrow8 = CurvedArrow(start_point=xg2_box.get_bottom(), end_point=blend_box.get_top(), angle=PI / 8,
-                             color="#FEE20B")
-        arrow9 = CurvedArrow(start_point=xg3_box.get_bottom(), end_point=blend_box.get_top(), angle=-PI / 8,
-                             color="#FEE20B")
-        arrow10 = CurvedArrow(start_point=xg4_box.get_bottom(), end_point=blend_box.get_right(), angle=-PI / 4,
-                              color="#FEE20B")
-        arrow11 = CurvedArrow(start_point=nn_box.get_bottom(), end_point=blend_box.get_left(), color="#FEE20B")
-        arrow_group = VGroup(arrow1, arrow2, arrow3, arrow4,
-                             arrow5, arrow7, arrow8,
-                             arrow9, arrow10, arrow11)
-        box_group = VGroup(blend_box, nn_box, xg1_box, xg2_box, xg3_box, xg4_box, df1_box, df2_box)
-        df_highlight = SurroundingRectangle(dfs_group, buff=0.25, stroke_color="#FEE20B", fill_color="#FEE20B")
-        xg_highlight = SurroundingRectangle(xg_group, buff=0.25, stroke_color="#FEE20B", fill_color="#FEE20B")
-        nn_highlight = SurroundingRectangle(nn_text, stroke_color="#FEE20B", fill_color="#FEE20B", buff=0.25)
-        df_info_rect = Rectangle(stroke_color="#fff0d5", fill_color="#211A1E", fill_opacity=0.85, height=7,
-                                 width=4).shift(5 * RIGHT)
-        df1_title = Text("Preprocessed").next_to(df_info_rect.get_top(), DOWN).scale(0.75)
-        df1_info = Paragraph("- MICE Imputation",
-                             "- One-Hot Encoding",
-                             "- Horizontal Dist. Var.",
-                             "- Outliers Removed",
-                             line_spacing=2).next_to(df1_title, DOWN).scale(0.5)
-        df2_title = Text("Standardized").next_to(df_info_rect.get_top(), DOWN).scale(0.75)
-        df2_info = Paragraph("- Used TensorFlow",
-                             "- Gaussian Proximity",
-                             "- Horizontal Dist. Var.",
-                             "- Normalized Data",
-                             line_spacing=2).next_to(df2_title, DOWN).scale(0.5)
-        self.play(Write(model_text), Write(box_group), Write(dfs_group), Write(model_group), Write(blend_text),
-                  Write(arrow_group))
-        self.wait(3)
-        self.play(DrawBorderThenFill(df_highlight))
+        md_title = Text("Model Architecture", font_size=50).shift(2.5 * UP)
+        md_title_border = SurroundingRectangle(md_title, buff=0.4, stroke_width=10, fill_opacity=0.5)
+        md_text = Paragraph("1. Convolutional Graph Neural Network",
+                            "2. Flattening Layers",
+                            "3. Fully Connected Layers",
+                            t2c={"1.": "#D62828", "2.": "#D62828", "3.": "#D62828"},
+                            font_size=45).shift(DOWN)
+        self.play(DrawBorderThenFill(md_title_border), Write(md_title), Write(md_text))
         self.wait(1)
-        self.play(FadeIn(df_info_rect, df1_info, df1_title, shift=LEFT))
-        self.wait(1)
-        self.play(ReplacementTransform(df1_info, df2_info), ReplacementTransform(df1_title, df2_title))
-        self.wait(1)
-        self.play(FadeOut(df_info_rect, df2_info, df2_title, shift=RIGHT))
-        self.wait(1)
-        self.play(DrawBorderThenFill(xg_highlight), Unwrite(df_highlight))
-        self.wait(1)
-        self.play(DrawBorderThenFill(nn_highlight), Unwrite(xg_highlight))
-        self.wait(1)
-        self.play(Unwrite(model_text, run_time=1), Unwrite(box_group, run_time=1),
-                  Unwrite(dfs_group, run_time=1), Unwrite(model_group, run_time=1),
-                  Unwrite(blend_text, run_time=1), Unwrite(arrow_group, run_time=1),
-                  Unwrite(nn_highlight, run_time=1))
+        self.play(Uncreate(md_title_border), Unwrite(md_title), Unwrite(md_text))
         self.wait(1)
 
 
@@ -540,15 +450,14 @@ class Animation9InsightsConclusion(Scene):
 
         # Conclusion
         results_title = Text("Conclusion", font_size=50, color="#007991").shift(2.5 * UP)
-        results_text = Paragraph("Our GNN Model Achieved", "a Validation Accuracy of 94.93%!", font_size=60,
-                                 t2c={"94.93%!": "#D62828", "GNN Model": "#D62828"})
+        results_text = Paragraph("Our Convolutional GNN Model Achieved", "a Validation Accuracy of 38.5%!",
+                                 font_size=40, t2c={"38.5%": "#D62828", "Convolutional GNN": "#D62828"})
         conc_border = SurroundingRectangle(results_text, buff=0.5, stroke_width=10)
         self.play(DrawBorderThenFill(results_title), Write(results_text), DrawBorderThenFill(conc_border))
         self.wait(1)
         analysis_text = Paragraph("Future Work May Look Into Adding Additional",
                                   "Features Beyond EEG Signals.",
-                                  font_size=40, t2c={"vertical depth": " #1B264F", "location": " #1B264F",
-                                                     "horizontal length": " #1B264F"})
+                                  font_size=40)
         analysis_border = SurroundingRectangle(analysis_text, buff=0.5, stroke_width=10)
         self.play(ReplacementTransform(results_text, analysis_text), ReplacementTransform(conc_border, analysis_border))
         self.wait(1)
