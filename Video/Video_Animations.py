b653951a9a1582557393d17200ff89985e725279
Benjamin Allen <110572177+benwillallen@users.noreply.github.com>
2025-02-01 18:21:24 -0600

    Finished Animation 4
    Signed-off-by: Benjamin Allen <110572177+benwillallen@users.noreply.github.com>

--- Video/Video_Animations.py
+++ Video/Video_Animations.py
@@ -42,6 +42,15 @@ class Animation1TitleMotivation(Scene):
                   Unwrite(title), FadeOut(by_text, shift=DOWN))
 
         # Motivation
+        motivation_text = Paragraph("-- Mental health contributes to ~14.3% deaths worldwide",
+                                    "-- Accurate, early identification is invaluable in treatment",
+                                    "-- Electroencephalogram (EEG) analysis has recently shown promise",
+                                    t2c={"Electroencephalogram (EEG)": "#D62828", "~14.3%": "#D62828"})
+        background_text = Paragraph("-- EEGs record brainwave activity over time",
+                                    "-- Measure electrical changes of neurons",
+                                    "-- Electrodes located at specific places on patient's head",
+                                    t2c={"EEG": "#D62828"})
+        eeg_image = ImageMobject("ce04b6a2-ed34-4757-8d2a-ba80aa0ab38b.jpg")
 
 
 class Animation3Objective(Scene):
@@ -65,12 +74,12 @@ class Animation4DataDescription(Scene):
                                         "age": "#1B264F", "sex": "#1B264F", "IQ": "#1B264F",
                                         "education level": "#1B264F",
                                         "Power spectral density (PSD)": "#D62828", "Coherence": "#D62828"},
-                                   font_size=40)
-        psd_text = Text("Power spectral density (PSD)").shift(4*UP)
+                                   font_size=40).scale(0.95)
+        psd_text = Text("Power spectral density (PSD)").shift(3.5*UP)
         self.add(b_plane)
         self.play(DrawBorderThenFill(title_border), Write(title), Write(data_desc_para, run_time=3))
         self.wait(2)
-        self.play(ReplacementTransform(data_desc_para, psd_text), VGroup(title, title_border).animate.shift(3*LEFT))
+        self.play(ReplacementTransform(data_desc_para, psd_text), FadeOut(title, title_border, shift=4*LEFT))
         self.wait(1)
 
         # Visual EEG Explanation
@@ -80,8 +89,8 @@ class Animation4DataDescription(Scene):
             .next_to(brain, LEFT, buff=0)
         ear_right = Circle(radius=0.5, stroke_width=10, stroke_color="#D62828", fill_color="#DF5353", fill_opacity=0.5)\
             .next_to(brain, RIGHT, buff=0)
-        eeg_verts = ["Fp1", "Fp2", "Fz", "Cz", "Pz", "F3", "C3", "P3", "O1", "F4", "C4", "P4", "02",
-                                    "F7", "T3", "T5", "F8", "T4", "T6", "A1", "A2"]
+        eeg_verts = ["Fp1", "Fp2", "Fz", "Cz", "Pz", "F3", "C3", "P3", "O1", "F4", "C4", "P4", "02", "F7",
+                     "T3", "T5", "F8", "T4", "T6", "A1", "A2"]
         eeg_edges = [(eeg_verts[idx], eeg_verts[j]) for idx in range(len(eeg_verts)) for j in range(idx+1, len(eeg_verts))]
         mix_graph = Graph(vertices=eeg_verts,
                           edges=eeg_edges,
@@ -104,13 +113,90 @@ class Animation4DataDescription(Scene):
         brain_text = Text("Brain", font_size=40).move_to(brain)
         nasion_text = Text("Nasion", font_size=40).next_to(brain, UP).scale(0.75)
         inion_text = Text("Inion", font_size=40).next_to(brain, DOWN).scale(0.75)
-        coh_text = Text("Coherence")
+
         self.wait(1)
         self.play(AnimationGroup(DrawBorderThenFill(brain), Write(brain_text), DrawBorderThenFill(ear_left),
                                  DrawBorderThenFill(ear_right), Write(nasion_text), Write(inion_text),
-                                 Write(mix_graph)))
+                                 Write(mix_graph), lag_ratio=0.2))
         self.wait(1)
-        #self.play(TransformMatchingShapes(mix_graph, eeg_graph))
+        self.play(ReplacementTransform(mix_graph, eeg_graph, run_time=2))
+        self.wait(1)
+
+        # Display concept of power spectrum density
+        bands_table = Table([["Delta", "0.5–4 Hz"],
+                             ["Theta", "4–7 Hz"],
+                             ["Alpha", "8–13 Hz"],
+                             ["Beta", "13–30 Hz"],
+                             ["Gamma", "30–80 Hz"],
+                             ["High Gamma", "80–150 Hz"]],
+                            col_labels=[Text("Band"), Text("Frequencies")]).scale(0.5)
+        bands_table.add_highlighted_cell((1, 1), color=GREEN)
+        bands_table.add_highlighted_cell((1, 2), color="#1B264F")
+        bands_table_border = SurroundingRectangle(bands_table, buff=0, fill_opacity=0.15, stroke_color="#21201E")
+        bands_table_source = Paragraph("Source: Nayak CS, Anilkumar AC. EEG Normal Waveforms.",
+                                       "[Updated 2023 Jul 24]. In: StatPearls [Internet].",
+                                       "Treasure Island (FL): StatPearls Publishing; 2025 Jan-.",
+                                       "Available from: https://www.ncbi.nlm.nih.gov/books/NBK539805/")\
+            .scale(0.3).next_to(bands_table, DOWN, buff=0.2)
+        main_table_border = SurroundingRectangle(VGroup(bands_table_border, bands_table_source),
+                                                 stroke_width=10, fill_opacity=0.75)
+        bands_table_group = VGroup(main_table_border, bands_table, bands_table_border, bands_table_source)\
+            .move_to(ORIGIN)
+        eeg_psd_graph = Graph(vertices=eeg_verts,
+                              edges=eeg_edges,
+                              vertex_config={"fill_color": YELLOW, "color": "#1B264F", "stroke_opacity": 1,
+                                             "stroke_width": 2, "radius": 0.475},
+                              edge_config={"stroke_opacity": 0.2, "color": "#21201E", "stroke_width": 3},
+                              layout={"Fp1": [1, 3.8, 0], "Fp2": [-1, 3.8, 0], "Fz": [0, 2, 0], "Cz": [0, 0, 0],
+                                      "Pz": [0, -2, 0],
+                                      "F3": [1.30, 2, 0], "C3": [1.9, 0, 0], "P3": [-1.30, -2, 0], "O1": [1, -3.8, 0],
+                                      "F4": [-1.30, 2, 0],
+                                      "C4": [-1.9, 0, 0], "P4": [1.30, -2, 0], "02": [-1, -3.8, 0], "F7": [2.30, 2.6, 0],
+                                      "T3": [3, 0, 0],
+                                      "T5": [2.3, -2.6, 0], "F8": [-2.3, 2.6, 0], "T4": [-3, 0, 0], "T6": [-2.3, -2.6, 0],
+                                      "A1": [4.5, 0, 0], "A2": [-4.5, 0, 0]},
+                              labels=True).scale(0.5)
+        psd_explain_text1 = Text("Measures Strength of Band", font_size=40).next_to(bands_table_group, DOWN)
+        psd_explain_text2 = Text("Measured by Electrode", font_size=40).next_to(bands_table_group, DOWN)
+        self.play(FadeIn(psd_explain_text1, shift=UP))
+        self.wait(1)
+        self.play(Write(bands_table_group), run_time=1.5)
+        self.wait(2)
+        self.play(Uncreate(bands_table_group), ReplacementTransform(psd_explain_text1, psd_explain_text2))
+        self.wait(1)
+        self.play(ReplacementTransform(eeg_graph, eeg_psd_graph))
+        self.wait(1)
+        self.play(FadeOut(psd_explain_text2, shift=DOWN))
+        self.wait(1)
+
+        # Display what coherence is
+        coh_text = Text("Coherence").move_to(psd_text)
+        coh_explain_text1 = Text("Synchronization Between Signals").move_to(psd_explain_text1)
+        coh_explain_text2 = Text("Measured by Edge").move_to(psd_explain_text1)
+        eeg_coh_graph = Graph(vertices=eeg_verts,
+                              edges=eeg_edges,
+                              vertex_config={"fill_color": "#fff0d5", "color": "#1B264F", "stroke_opacity": 1,
+                                             "stroke_width": 2, "radius": 0.475},
+                              edge_config={"stroke_opacity": 0.2, "color": YELLOW, "stroke_width": 3},
+                              layout={"Fp1": [1, 3.8, 0], "Fp2": [-1, 3.8, 0], "Fz": [0, 2, 0], "Cz": [0, 0, 0],
+                                      "Pz": [0, -2, 0],
+                                      "F3": [1.30, 2, 0], "C3": [1.9, 0, 0], "P3": [-1.30, -2, 0], "O1": [1, -3.8, 0],
+                                      "F4": [-1.30, 2, 0],
+                                      "C4": [-1.9, 0, 0], "P4": [1.30, -2, 0], "02": [-1, -3.8, 0],
+                                      "F7": [2.30, 2.6, 0],
+                                      "T3": [3, 0, 0],
+                                      "T5": [2.3, -2.6, 0], "F8": [-2.3, 2.6, 0], "T4": [-3, 0, 0],
+                                      "T6": [-2.3, -2.6, 0],
+                                      "A1": [4.5, 0, 0], "A2": [-4.5, 0, 0]},
+                              labels=True).scale(0.5)
+        self.play(ReplacementTransform(psd_text, coh_text), FadeIn(coh_explain_text1, shift=UP))
+        self.wait(1)
+        self.play(ReplacementTransform(coh_explain_text1, coh_explain_text2),
+                  ReplacementTransform(eeg_psd_graph, eeg_coh_graph))
+        self.wait(2)
+        self.play(Unwrite(eeg_coh_graph), FadeOut(coh_text, shift=UP), FadeOut(coh_explain_text2, shift=DOWN),
+                  Unwrite(nasion_text), Unwrite(inion_text), Unwrite(brain_text), Uncreate(brain), Uncreate(ear_left),
+                  Uncreate(ear_right))
         self.wait(1)
 
 
@@ -140,7 +226,7 @@ class Animation8Evaluation(Scene):
         # and most importantly confusion matrix)
 
 
-class Animation9Insights(Scene):
+class Animation9InsightsConclusion(Scene):
     def construct(self):
         b_plane = set_defaults()
         # Include any cool visualizations here can move these up if they help explain any of the preprocessing or
